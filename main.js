a658affb2f5c9f89195f924cf0f96ad54ed2c61e
10o0010o <124211498+10o0010o@users.noreply.github.com>
2023-07-03 18:09:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX = 0;
+noseY = 0;
+
+
+function preload()
+{
+  clownNose = loadImage('https://i.postimg.cc/vBB2WzDB/appa.png');
+}
+
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clownNose, noseX, noseY, 70, 50);
+
+}
+
+
+function takeSnapshot() {
+  save('myFilterImage.png');
+}
+
+
+function modelLoaded() {
+  console.log('PoseNet foi inicializado');
+}
+
+
+function gotPoses(results) {
+  if (results.length > 0) {
+    noseX = results[0].pose.nose.x - 35;
+    noseY = results[0].pose.nose.y - 10;
+  }
+}
